4580ced1aa198fbaca8121711e555f428a396355
aniketk03 <aniketkshirsagar05@gmail.com>
2024-04-11 18:13:25 +0530

    autowire in interface

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -7,35 +7,35 @@ public class First {
     private int a;
     private Computer com;
 
-    public First(int age, Laptop lap, int a)
-    {
-        System.out.println(age);
-        System.out.println(lap);
-        System.out.println(a);
-        System.out.println("Hello");
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-    public void setlap(Laptop lap)
-
-        {
-            this.lap = lap;
-        }
-
-        public Laptop getlap()
-        {
-            return lap;
-        }
-
-        public Computer getcom()
-        {
-         return com;
-        }
+//    public First(int age, Laptop lap, int a)
+//    {
+//        System.out.println(age);
+//        System.out.println(lap);
+//        System.out.println(a);
+//        System.out.println("Hello");
+//    }
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//    public void setlap(Laptop lap)
+//
+//        {
+//            this.lap = lap;
+//        }
+//
+//        public Laptop getlap()
+//        {
+//            return lap;
+//        }
+//
+//        public Computer getcom()
+//        {
+//         return com;
+//        }
 
         public void setcom(Computer com)
         {
