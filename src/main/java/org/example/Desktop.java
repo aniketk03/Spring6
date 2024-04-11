f15f4e6258b448b45d6db59d3eb745e4d430bd44
aniketk03 <aniketkshirsagar05@gmail.com>
2024-04-11 19:21:55 +0530

    Lazy Bean

--- src/main/java/org/example/Desktop.java
+++ src/main/java/org/example/Desktop.java
@@ -2,7 +2,10 @@ package org.example;
 
 public class Desktop implements Computer {
 
-
+public Desktop()
+{
+    System.out.println("this is desktop  constructor");
+}
     @Override
     public void play() {
         System.out.println("Music Playing in Desktop");
